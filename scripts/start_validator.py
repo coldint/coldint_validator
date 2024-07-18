6659345f6fca6603de8b4550da9e9162311e7e24
Reinier Heeres <reinier@coldint.io>
2024-07-18 08:05:11 +0000

    Update start_validator.py

--- scripts/start_validator.py
+++ scripts/start_validator.py
@@ -156,11 +156,11 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
         description="Automatically update and restart the validator process when a new version is released.",
-        epilog="Example usage: python start_validator.py --pm2_name 'net9vali' --wallet_name 'wallet1' --wallet_hotkey 'key123'",
+        epilog="Example usage: python start_validator.py --pm2_name 'sn29vali' --wallet_name 'wallet1' --wallet_hotkey 'key123'",
     )
 
     parser.add_argument(
-        "--pm2_name", default="net9vali", help="Name of the PM2 process."
+        "--pm2_name", default="sn29vali", help="Name of the PM2 process."
     )
 
     flags, extra_args = parser.parse_known_args()
