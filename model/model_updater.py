89266a7a1ff91735625952800dc8aeff87781c57
Reinier Heeres <reinier@coldint.io>
2024-09-08 07:09:05 +0000

    model_updater: return error code, don't use model_tracker
    - force flag now has to be determined elsewhere

--- model/model_updater.py
+++ model/model_updater.py
@@ -5,7 +5,6 @@ import os
 from model import competitions
 from model.model_utils import get_hash_of_two_strings
 from model.data import ModelMetadata
-from model.model_tracker import ModelTracker
 from model.storage.disk import utils
 from model.storage.local_model_store import LocalModelStore
 from model.storage.model_metadata_store import ModelMetadataStore
@@ -15,52 +14,50 @@ from transformers import AutoModelForCausalLM
 class ModelUpdater:
     """Checks if the currently tracked model for a hotkey matches what the miner committed to the chain."""
 
+    SYNC_RESULT_SUCCESS             = 0
+    SYNC_RESULT_ERROR               = -1
+    SYNC_RESULT_DOWNLOAD_FAILED     = -2
+    SYNC_RESULT_HASH_FAILED         = -3
+    SYNC_RESULT_MODEL_NOT_ALLOWED   = -4
+    RETRY_RESULTS = {
+        SYNC_RESULT_ERROR,
+        SYNC_RESULT_DOWNLOAD_FAILED,
+    }
+
     def __init__(
         self,
         remote_store: RemoteModelStore,
         local_store: LocalModelStore,
-        model_tracker: ModelTracker,
         comps: dict,
     ):
         self.remote_store = remote_store
         self.local_store = local_store
-        self.model_tracker = model_tracker
         self.competitions = comps
 
     def set_competitions(self, comp):
         self.competitions = comp
 
-    async def sync_model(self, hotkey: str, metadata, force: bool = False) -> bool:
+    async def sync_model(self, hotkey: str, metadata) -> bool:
         """Updates local model for a hotkey if out of sync and returns if it was updated.
 
         Args:
            hotkey (str): The hotkey of the model to sync.
-           force (bool): Whether to force a sync for this model, even if it's chain metadata hasn't changed.
         """
 
         if self.competitions is None:
             bt.logging.debug("Competitions not known")
-            return False
+            return self.SYNC_RESULT_ERROR
 
         if not metadata:
-            return False
+            return self.SYNC_RESULT_ERROR
 
         if metadata.id.competition not in self.competitions:
             bt.logging.trace(
                 f"Hotkey {hotkey} advertized model for invalid competition {metadata.id.competition}"
             )
-            return False
+            return self.SYNC_RESULT_ERROR
         cname = metadata.id.competition
         cparams = self.competitions[cname]
-
-        # Check what model id the model tracker currently has for this hotkey.
-        tracker_model_metadata = self.model_tracker.get_model_metadata_for_miner_hotkey(
-            hotkey
-        )
-
-        # If we are not forcing a sync due to retrying a top model we can short-circuit if no change.
-        if not force and metadata == tracker_model_metadata:
-            return False
 
         # Get the local path based on the local store to download to (top level hotkey path)
         model_available = False
@@ -86,7 +83,7 @@ class ModelUpdater:
             if bytes_free < bytes_free_required:
                 gb_total = int(statvfs.f_frsize*statvfs.f_blocks//1e9)
                 bt.logging.warning(f'not downloading model {metadata.id}: {bytes_free/1e9:.1f} of {gb_total} GB free while {bytes_free_required/1e9:.1f} GB required')
-                return False
+                return self.SYNC_RESULT_DOWNLOAD_FAILED
             path = self.local_store.get_path(hotkey)
             bt.logging.debug(f"download_model({metadata.id}) to {path}")
             try:
@@ -94,19 +91,18 @@ class ModelUpdater:
                     metadata.id, path, model_size_limit
                 )
             except Exception as e:
-                bt.logging.trace(
+                bt.logging.debug(
                     f"Failed to download model for hotkey {hotkey} due to {e}."
                 )
-                return False
+                return self.SYNC_RESULT_DOWNLOAD_FAILED
 
             # Check that the hash of the downloaded content matches.
             hash_with_hotkey = get_hash_of_two_strings(model.id.hash, hotkey)
             if hash_with_hotkey != metadata.id.hash:
                 bt.logging.trace(
-                    f"Sync for hotkey {hotkey} failed. Hash of content downloaded from hugging face {model.id.hash} "
-                    + f"or the hash including the hotkey {hash_with_hotkey} do not match chain metadata {metadata}."
+                    f"Sync for hotkey {hotkey} failed. Hash of hugging face content and hotkey {hash_with_hotkey} does not match chain metadata {metadata}."
                 )
-                return False
+                return self.SYNC_RESULT_HASH_FAILED
             pt_model = model.pt_model
 
         # Check that the model parameters are allowed in the proposed competition
@@ -115,9 +111,6 @@ class ModelUpdater:
             bt.logging.trace(
                 f"Sync for hotkey {hotkey} failed: model not allowed in competition {cname}: {reason}"
             )
-            return False
+            return self.SYNC_RESULT_MODEL_NOT_ALLOWED
 
-        # Update the tracker
-        self.model_tracker.on_miner_model_updated(hotkey, metadata)
-
-        return True
+        return self.SYNC_RESULT_SUCCESS
