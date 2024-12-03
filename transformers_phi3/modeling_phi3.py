89935c794c6b61576e0b208593f4659b0bc03ebd
µ <maarten@coldint.io>
2024-12-03 00:24:41 +0000

    transformers_phi3: minimize CPU RAM usage by loading model slices from disk into GPU
    Verified to give identical losses using sliced vs regular eval.

--- transformers_phi3/modeling_phi3.py
+++ transformers_phi3/modeling_phi3.py
@@ -50,6 +50,7 @@ from transformers.utils import (
 )
 from .configuration_phi3 import Phi3Config
 
+from transformers import AutoModelForCausalLM
 
 if is_flash_attn_2_available():
     from transformers.modeling_flash_attention_utils import _flash_attention_forward
@@ -151,7 +152,8 @@ class Phi3RotaryEmbedding(nn.Module):
     @torch.no_grad()
     def forward(self, x, position_ids, seq_len=None):
         # x: [bs, num_attention_heads, seq_len, head_size]
-        self.inv_freq.to(x.device)
+        if self.inv_freq.device != x.device:
+            self.inv_freq = self.inv_freq.to(x.device)
         inv_freq_expanded = self.inv_freq[None, :, None].float().expand(position_ids.shape[0], -1, 1)
         position_ids_expanded = position_ids[:, None, :].float()
         # Force float32 since bfloat16 loses precision on long contexts
@@ -1199,14 +1201,19 @@ class SlicedPhi3ForCausalLMWrapper:
         Evaluate losses of samples on sliced model.
         """
         output_states = []
-        for slice_idx,model_slice in enumerate(self.slices):
-            model_slice = model_slice.to(self.device)
-            model_params = model_slice.num_parameters()
+        for slice_idx,model_slice_opts in enumerate(self.slices):
+            model_slice = AutoModelForCausalLM.from_pretrained(
+                device_map=model_slice_opts['device_map'],
+                pretrained_model_name_or_path=self.model.name_or_path,
+                attn_implementation=self.model.config._attn_implementation,
+                torch_dtype=self.model.dtype
+            )
+            for k,v in model_slice_opts['config'].items():
+                setattr(model_slice.config,k,v)
+            model_params = model_slice.num_loaded_parameters()
             state_size = sum([s.numel() for s in output_states if s is not None])
-            logger.info(f'evaluating slice {slice_idx}, n_params={model_params}, state_size={state_size}...')
+            logger.info(f'evaluating slice {slice_idx}, n_params={model_params/1e6:.01f}M, state_size={state_size}...')
             losses = self.evaluate_losses_slice(model_slice,samples=samples,output_states=output_states,reduction=reduction)
-            model_slice = model_slice.to('cpu')
-            torch.cuda.empty_cache()
         return losses
 
     def evaluate_losses_slice(self,model_slice,samples=None,output_states=[],reduction='mean'):
@@ -1271,23 +1278,29 @@ class SlicedPhi3ForCausalLMWrapper:
         return losses
 
     def gen_slices(self):
-        model_data = {}
-        # strip model, restore later
+        # generate map mapping all elements to meta device
+        meta_map = {}
         for pname, p in self.model.named_parameters():
-            model_data[pname] = p.data
-            p.data = torch.zeros(0)
+            meta_map[pname] = 'meta'
+            if p.device.type != 'meta':
+                p.data = torch.zeros(0,dtype=self.model.dtype) # dtype must be set or model dtype will magically change
 
         self.slices = []
         for i,layer_from in enumerate(self.start_layers[:-1]):
             layer_to = self.start_layers[i+1]
             logger.info(f'Generating slice #{i}: {layer_from}..{layer_to}')
+            slice_config = dict(
+                start_at_layer=layer_from,
+                return_states_at_layer=layer_to,
+            )
+            slice_device_map = meta_map.copy()
             params = self.filter_params(layer_from=layer_from,layer_to=layer_to)
-            model_slice = copy.deepcopy(self.model) # copy of empty model
-            for pname,p in model_slice.named_parameters():
-                if pname in params['param_names_kept']:
-                    p.data = model_data[pname]
-            model_slice.config.start_at_layer = layer_from
-            model_slice.config.return_states_at_layer = layer_to
+            for param in params['param_names_kept']:
+                slice_device_map[param] = self.device
+            model_slice = dict(
+                config=slice_config,
+                device_map=slice_device_map,
+            )
             self.slices.append(model_slice)
 
     @staticmethod
@@ -1355,6 +1368,13 @@ class SlicedPhi3ForCausalLM(Phi3PreTrainedModel):
         Generate a container object with this model sliced into pieces.
         """
         return SlicedPhi3ForCausalLMWrapper(model=self,**kwargs)
+
+    def num_loaded_parameters(self):
+        ret = 0
+        for p in self.parameters():
+            if p.device.type != 'meta':
+                ret += p.numel()
+        return ret
 
     # Copied from transformers.models.llama.modeling_llama.LlamaForCausalLM.get_input_embeddings
     def get_input_embeddings(self):
