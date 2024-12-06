63db5dac56e1ef14a842793a83175e1fb9bf9f7a
Reinier Heeres <reinier@coldint.io>
2024-12-06 10:40:37 +0000

    validator.py: fix logging --> bt.logging

--- neurons/validator.py
+++ neurons/validator.py
@@ -1450,7 +1450,7 @@ def check_and_compute_losses(
         allow_sliced = model_type in cinfo['model_types']
 
     if not allow_sliced:
-        logging.info(f'Model of type {model_type} cannot be sliced, reloading on device')
+        bt.logging.info(f'Model of type {model_type} cannot be sliced, reloading on device')
         # Not expected. Load model properly.
         model_i = local_store.retrieve_model(metadata.hotkey, metadata.id, path=metadata.path)
 
