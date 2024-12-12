545768616abf3cbe8bf381b142950690eadd8540
Reinier Heeres <reinier@coldint.io>
2024-12-12 10:38:05 +0000

    mathutils: deal with None values

--- utilities/mathutils.py
+++ utilities/mathutils.py
@@ -1,24 +1,34 @@
 import numpy as np
 
 def naninf_filt(ar):
+    if ar is None:
+        return None
     if not isinstance(ar, np.ndarray):
         ar = np.array(ar)
     mask = ~np.isnan(ar) & ~np.isinf(ar)
     return ar[mask]
 
 def naninf_count(ar):
+    if ar is None:
+        return 0
     ar = naninf_filt(ar)
     return len(ar)
 
 def naninf_mean(ar):
+    if ar is None:
+        return 0
     ar = naninf_filt(ar)
     return np.inf if len(ar) == 0 else np.mean(ar)
 
 def naninf_std(ar):
+    if ar is None:
+        return 0
     ar = naninf_filt(ar)
     return np.inf if len(ar) == 0 else np.std(ar)
 
 def naninf_equal(ar_a,ar_b,close=False):
+    if ar_a is None or ar_b is None:
+        return False
     if not isinstance(ar_a, np.ndarray):
         ar_a = np.array(ar_a)
     if not isinstance(ar_b, np.ndarray):
@@ -36,6 +46,8 @@ def naninf_close(ar_a,ar_b):
 
 # for informative purposes only
 def naninf_meandelta(ar_a,ar_b):
+    if ar_a is None or ar_b is None:
+        return np.inf
     if not isinstance(ar_a, np.ndarray):
         ar_a = np.array(ar_a)
     if not isinstance(ar_b, np.ndarray):
